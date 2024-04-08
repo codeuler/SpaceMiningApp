1517d6b2100459787bd02f7bb234d51efd8c0eca
codeuler <159579558+codeuler@users.noreply.github.com>
2024-04-08 12:16:57 -0500

    desaparecer la toolbar en el entry
    EntryFragment.kt - Retirar los comentarios innecesarios en
    Activity Main - Cree el listener para que se desaparezca la toolbar en la pantalla principal y cambie la funcion del boton de retroceso por navigate up
    fragment_entry - Quite atributos redundantes
    fragment_menu -  Quite atributos redundantes

--- app/src/main/java/com/example/spacemining/EntryFragment.kt
+++ app/src/main/java/com/example/spacemining/EntryFragment.kt
@@ -1,17 +1,12 @@
 package com.example.spacemining
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toolbar
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import com.example.spacemining.databinding.ActivityMainBinding
 import com.example.spacemining.databinding.FragmentEntryBinding
 
 class EntryFragment : Fragment() {
@@ -27,19 +22,6 @@ class EntryFragment : Fragment() {
             //Se realiza la trasición de este pantalla a menufragment
             view?.findNavController()?.navigate(R.id.action_entryFragment_to_menuFragment)
         }
-
-
-        //val activityBinding = (requireActivity() as MainActivity).ActivityMainBinding
-
-
-
-        //val actividadContenedora = requireActivity()
-        // Modificar vistas dentro de la actividad
-        //actividadContenedora.findViewById<TextView>(R.id.text_toolbar).text = "Cualquier cosa"
-
-        //binding.explorarBotton.setOnClickListener(
-        //    Navigation.createNavigateOnClickListener(R.id.action_entryFragment_to_menuFragment)
-        //)
 
         return binding.root
     }
--- app/src/main/java/com/example/spacemining/MainActivity.kt
+++ app/src/main/java/com/example/spacemining/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.spacemining
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
@@ -9,20 +10,33 @@ import com.example.spacemining.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
-    lateinit var bindingActivity:ActivityMainBinding
+    lateinit var bindingActivity: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val bindingActivity: ActivityMainBinding = DataBindingUtil.setContentView<ActivityMainBinding>(
-            this, R.layout.activity_main
-        )
+        val bindingActivity: ActivityMainBinding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(
+                this, R.layout.activity_main
+            )
+
+        //Obtengo el Contenedor de fragmentos
         val navHostController =
             supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        //Obtengo el navcontroller del contenedor de fragmentos
         navController = navHostController.navController
+        //Cada vez que se haga una transición de fragmentos se analiza si dicho fragmento es el entry
+        //De ser así se desapareche la tool var, de no ser así se pone visible
+        navController.addOnDestinationChangedListener { _, destination, _ ->
 
-        bindingActivity.backButton.setOnClickListener {
-            navController.popBackStack()
+            if (destination.id == R.id.entryFragment) {
+                bindingActivity.toolbar.visibility = View.GONE
+            } else {
+                bindingActivity.toolbar.visibility = View.VISIBLE
+            }
         }
 
 
+        bindingActivity.backButton.setOnClickListener {
+            navController.navigateUp()
+        }
     }
 }
