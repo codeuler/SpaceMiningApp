5fa52531c3ad296de3ba342fd5f967b11599be3c
Marcos Parra <marcos345dav@gmail.com>
2024-04-09 10:29:04 -0500

    Agregar url
    adicion de la url

--- app/src/main/java/com/example/spacemining/StufInSpaceFragment.kt
+++ app/src/main/java/com/example/spacemining/StufInSpaceFragment.kt
@@ -1,5 +1,7 @@
 package com.example.spacemining
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -18,6 +20,18 @@ class StufInSpaceFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentStufInSpaceBinding = DataBindingUtil.inflate(
             inflater,R.layout.fragment_stuf_in_space,container,false)
+
+        val url = "https://"+binding.linkStuff.text.toString()
+
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl(url)
+
+        binding.linkStuff.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
+
         return binding.root
     }
 
